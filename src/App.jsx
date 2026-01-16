3858ee2409af554b99eede772e0c764d4f910610
shivamYadav <shivambyadav99@gmail.com>
2026-01-16 21:27:00 +0530

    info updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Projects from './components/Projects';
 import Certifications from './components/Certifications';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Certifications />
         <Contact />
         <Footer />
+        <Analytics />
       </div>
     </div>
   );
